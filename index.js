3aeb8fd2ac331954cc583ccfac39e57929430c69
Dmitriy <dimka_it@list.ru>
2022-07-14 15:57:34 +0300

    Post request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const chalk = require('chalk');
 const fs = require('fs/promises');
 const path = require('path');
+const {addNote} = require('./notes.controller');
 
 const port = 3000;
 const basePath = path.join(__dirname, 'pages');
@@ -14,6 +15,23 @@ const server = http.createServer(async (req, res) => {
       'Content-Type': 'text/html'
     });
     res.end(content);
+  } else if (req.method === 'POST') {
+    const body = [];
+    res.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8'
+    });
+
+    req.on('data', (data) => {
+      console.log('data', data);
+      body.push(Buffer.from(data));
+    });
+
+    req.on('end', () => {
+      const title = body.toString().split('=')[1].replaceAll('+', ' ');
+      addNote(title);
+      res.end(`Title = ${title}`);
+    });
+
   }
 });
 
